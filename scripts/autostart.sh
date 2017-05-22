4fda4535fa7487844f8ec57e2fd18c7f42b77fec
AberDerBart <nonatz@web.de>
2017-05-22 22:49:47 +0200

    updated status bar

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 owncloud &
+nm-applet &
 bash ~/.config/notebookConfig/scripts/setBg.sh
-if [ -e ~/ownCloud/scripts/$HOSTNAME/autostart.sh ];then
-	bash ~/ownCloud/scripts/$HOSTNAME/autostart.sh
-fi
