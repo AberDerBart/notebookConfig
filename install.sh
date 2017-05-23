0afa65abb5adbb8b97fd9f7a763054e0f1cc303b
AberDerBart <nonatz@web.de>
2017-05-23 13:26:31 +0200

    added installation scripts

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function ask {
+	question=$1
+	read -r -p "${1} [y/N]: " REPLY
+
+	case $REPLY in
+		[yY]) return 0;;
+		*) return 1;;
+	esac
+
+}
+
+
+ask "Install i3 config?" && mkdir -p ~/.config/i3/ && cp i3config ~/.config/i3/config
+ask "Install i3status config?" && mkdir -p ~/.config/i3 && cp i3status ~/.config/i3status/config
