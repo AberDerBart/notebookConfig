94296a802f98f31d5cffca124ea855a64083563d
AberDerBart <nonatz@web.de>
2017-05-25 11:26:12 +0200

    added scripts installation to the folder ~/.software/scripts

--- install.sh
+++ install.sh
@@ -11,9 +11,26 @@ function ask {
 
 }
 
-
-ask "Install i3 config?" && mkdir -p ~/.config/i3/ && ln -s $PWD/i3config ~/.config/i3/config
-ask "Install i3status config?" && mkdir -p ~/.config/i3 && ln -s $PWD/i3status ~/.config/i3status/config
-ask "Install .bashrc?" && ln -s $PWD/bashrc ~/.bashrc
-ask "Install .taskrc?" && ln -s $PWD/taskrc ~/.taskrc
-ask "Install .taskrc?" && ln -s $PWD/taskrc ~/.taskrc
+if ask "Install i3 config?"; then
+	set -o xtrace
+        mkdir -p ~/.config/i3/
+        ln -s $PWD/i3config ~/.config/i3/config
+	set +o xtrace
+fi
+if ask "Install i3status config?"; then
+	set -o xtrace
+        mkdir -p ~/.config/i3
+	ln -s $PWD/i3status ~/.config/i3status/config
+	set +o xtrace
+fi
+if ask "Install .bashrc?"; then
+	set -o xtrace
+       	ln -s $PWD/bashrc ~/.bashrc
+	set +o xtrace
+fi
+if ask "Install scripts?"; then
+	set -o xtrace
+       	mkdir -p ~/.software 
+	ln -s $PWD/scripts ~/.software/
+	set +o xtrace
+fi
