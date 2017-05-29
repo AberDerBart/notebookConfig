a5652ec0b7bbcc283804741618c546b65c7b6eab
AberDerBart <nonatz@web.de>
2017-05-29 23:36:32 +0200

    now force-linking all symlinks

--- install.sh
+++ install.sh
@@ -4,28 +4,28 @@ source scripts/ask.sh
 if ask "Install i3 config?"; then
 	set -o xtrace
         mkdir -p ~/.config/i3/
-        ln -s $PWD/i3config ~/.config/i3/config
+        ln -fs $PWD/i3config ~/.config/i3/config
 	set +o xtrace
 fi
 if ask "Install i3status config?"; then
 	set -o xtrace
         mkdir -p ~/.config/i3
-	ln -s $PWD/i3status ~/.config/i3status/config
+	ln -fs $PWD/i3status ~/.config/i3status/config
 	set +o xtrace
 fi
 if ask "Install .bashrc?"; then
 	set -o xtrace
-       	ln -s $PWD/bashrc ~/.bashrc
+       	ln -fs $PWD/bashrc ~/.bashrc
 	set +o xtrace
 fi
 if ask "Install .taskrc?"; then
 	set -o xtrace
-	ln -s $PWD/taskrc ~/.taskrc
+	ln -fs $PWD/taskrc ~/.taskrc
 	set +o xtrace
 fi
 if ask "Install scripts?"; then
 	set -o xtrace
        	mkdir -p ~/.software 
-	ln -s $PWD/scripts ~/.software/
+	ln -fs $PWD/scripts ~/.software/
 	set +o xtrace
 fi
