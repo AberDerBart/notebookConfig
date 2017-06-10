0ff110da7d74294c5cf9b54bf72f7a617f6e2b0c
AberDerBart <nonatz@web.de>
2017-06-10 02:35:45 +0200

    improved mpd status script

--- scripts/mpdStatus.py
+++ scripts/mpdStatus.py
@@ -6,14 +6,14 @@ HOST="faultier"
 HOST=os.environ["MPD_HOST"]
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
 
 stateDict={"play":u'\u25BA',
 	"pause":u'\u2161',
@@ -22,14 +22,26 @@ stateDict={"play":u'\u25BA',
 c=mpd.MPDClient()
 
 try:
+	outString=HOST
+
 	c.connect(HOST,6600)
-	state=c.status()["state"]
+	
+
+	status=c.status()
+
+	if "volume" in status:
+		outString+=" "+str(status["volume"]+"%")
+
+
+	outString+=": "+stateDict[status["state"]]
+
 	current=c.currentsong()
 	if(len(current)):
 		formattedSong=current["artist"] + " - " + current["album"] + " - " + current["title"]
-		print(HOST+": "+stateDict[state]+" "+formattedSong)
-	else:
-		print(HOST+": "+stateDict[state])
+		outString+=" "+formattedSong
+
+	print(outString)
+	
 	c.close()
 except:
 	print(HOST)
