88914605668ebf5fb663146e0e5d7cc25c9c2cf5
AberDerBart <nonatz@web.de>
2017-11-10 14:34:37 +0100

    added dmenu for selecting monitor

---
+++ scripts/screen.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+DISPLAYS=$(xrandr|grep " connected"|cut -d " " -f 1) 
+
+if SELECTED=$(echo $DISPLAYS|tr -s " " "\n"|dmenu -i)
+then
+	echo setting $SELECTED to auto
+	xrandr --output $SELECTED --auto
+	
+	OTHERS=$(echo $DISPLAYS|tr -s " " "\n"|grep -v $SELECTED)
+
+	if [ -n "$OTHERS" ]
+	then
+		echo $OTHERS
+		for OTHER in $OTHERS
+		do
+			xrandr --output $OTHER --off
+			echo turning $OTHER off
+		done
+	fi
+
+else
+	echo canceled
+fi
+
+
+
