0cddcd46cb8c0ce591806bb504a9aa4f942581be
AberDerBart <nonatz@web.de>
2017-12-05 19:02:11 +0100

    adjusted spaceState for use with i3blocks

--- scripts/spaceState.py
+++ scripts/spaceState.py
@@ -8,8 +8,14 @@ if(req.status_code == 200):
 	spaceState=json.loads(req.text)
 	openState=spaceState["isOpen"]
 	if(openState):
-		print("SpaceState: Open (" + str(spaceState["openedBy"]) +")")
+		print("S0: "+str(spaceState["openedBy"]))
+		print("S0: "+str(spaceState["openedBy"]))
+		print("#00ff00")
 	else:
-		print("SpaceState: Closed")
+		print("S0: Closed")
+		print("S0: Closed")
+		print("#ff0000")
 else:
-	print("Error")
+	print()
+	print()
+	print("#000000")
