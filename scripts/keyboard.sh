3e9b537e02b81d7ccf1580e89ae5202c4cecbea1
AberDerBart <nonatz@web.de>
2017-12-07 15:47:17 +0100

    added us keyboard layout

--- scripts/keyboard.sh
+++ scripts/keyboard.sh
@@ -4,7 +4,8 @@ LAYOUT=$(setxkbmap -print|grep xkb_symbols|cut -d "+" -f2)
 
 function toggleLayout {
 	case $LAYOUT in
-		"de") setxkbmap ara;;
+		"de")  setxkbmap ara;;
+		"ara") setxkbmap -rules evdev -model evdev -layout us -variant altgr-intl -option ctrl:nocaps;;
 		*) setxkbmap de;;
 	esac
 	LAYOUT=$(setxkbmap -print|grep xkb_symbols|cut -d "+" -f2)
