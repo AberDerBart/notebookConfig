10889dbdf2ee513e4cee2fcb80eb4c3c36a2cd8f
AberDerBart <nonatz@web.de>
2018-01-10 18:45:06 +0100

    introduce mpdforward

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 owncloud &
 nm-applet &
+python ~/.software/mpdforward/mpdforward.py &
 bash ~/.config/notebookConfig/scripts/setBg.sh
--- scripts/setMpdHost.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "export MPD_HOST=$1" > ~/.mpcrc
-pkill -RTMIN+2 i3blocks
