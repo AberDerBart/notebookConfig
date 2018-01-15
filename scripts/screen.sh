0a6d66750ae81e1d9aa78038e1b1d2da76277d9a
AberDerBart <nonatz@web.de>
2018-01-15 11:32:52 +0100

    add enable/disable feature if only one display is connected

--- scripts/screen.sh
+++ scripts/screen.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 DISPLAYS=$(xrandr|grep " connected"|cut -d " " -f 1) 
+ACTIVE=$(xrandr|grep " connected"|grep [0-9]*x[0-9]*+[0-9]*+[0-9]*|cut -d " " -f 1)
 
-if SELECTED=$(echo $DISPLAYS|tr -s " " "\n"|dmenu -i)
+NUMBER=$(echo $DISPLAYS|wc -w)
+
+if [[ "$NUMBER" == "1" ]]
 then
-	echo setting $SELECTED to auto
-	xrandr --output $SELECTED --auto
-	
-	OTHERS=$(echo $DISPLAYS|tr -s " " "\n"|grep -v $SELECTED)
-
-	if [ -n "$OTHERS" ]
+	if [[ $ACTIVE ]]
 	then
-		echo $OTHERS
-		for OTHER in $OTHERS
-		do
-			xrandr --output $OTHER --off
-			echo turning $OTHER off
-		done
+		xrandr --output $DISPLAYS --off
+		echo "turning $DISPLAYS off"
+	else
+		xrandr --output $DISPLAYS --auto
+		echo "turning $DISPLAYS on"
 	fi
-
 else
-	echo canceled
+	if SELECTED=$(echo $DISPLAYS|tr -s " " "\n"|dmenu -i)
+	then
+		echo setting $SELECTED to auto
+		xrandr --output $SELECTED --auto
+		
+		OTHERS=$(echo $DISPLAYS|tr -s " " "\n"|grep -v $SELECTED)
+
+		if [ -n "$OTHERS" ]
+		then
+			echo $OTHERS
+			for OTHER in $OTHERS
+			do
+				xrandr --output $OTHER --off
+				echo turning $OTHER off
+			done
+		fi
+
+	else
+		echo canceled
+	fi
 fi
 
 bash ~/.software/scripts/setBg.sh
