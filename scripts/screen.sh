6bfcd7194f3740d4ee4ac6d8479f7660f1db474d
AberDerBart <nonatz@web.de>
2018-11-13 00:04:00 +0100

    improve screen change

--- scripts/screen.sh
+++ scripts/screen.sh
@@ -1,34 +1,13 @@
 #!/bin/bash
-DISPLAYS=$(xrandr|grep " connected"|cut -d " " -f 1) 
-ACTIVE=$(xrandr|grep " connected"|grep "[0-9]*x[0-9]*+[0-9]*+[0-9]*"|cut -d " " -f 1)
 
-NUMBER=$(echo $DISPLAYS|wc -w)
+PROFILES=$(basename -a $(ls ~/.software/scripts/xrandr))
 
-if [[ "$NUMBER" == "1" ]]
+if SELECTED=$(basename -a $(ls ~/.software/scripts/xrandr) |dmenu -i)
 then
-	echo autoconfiguring displays
-	xrandr --auto
+	echo "Selecting profile $SELECTED"
+	bash -c "bash ~/.software/scripts/xrandr/${SELECTED}"
 else
-	if SELECTED=$(echo $DISPLAYS|tr -s " " "\n"|dmenu -i)
-	then
-		echo setting $SELECTED to auto
-		xrandr --output $SELECTED --auto
-		
-		OTHERS=$(echo $DISPLAYS|tr -s " " "\n"|grep -v $SELECTED)
-
-		if [ -n "$OTHERS" ]
-		then
-			echo $OTHERS
-			for OTHER in $OTHERS
-			do
-				xrandr --output $OTHER --off
-				echo turning $OTHER off
-			done
-		fi
-
-	else
-		echo canceled
-	fi
+	echo "canceled"
 fi
 
 bash ~/.software/scripts/setBg.sh
