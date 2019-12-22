769d972833a68dab902ead720dbd292155db6033
AberDerBart <nonatz@web.de>
2019-12-22 18:10:47 +0100

    Revert "introduce mpdforward"
    This reverts commit 10889dbdf2ee513e4cee2fcb80eb4c3c36a2cd8f.

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 owncloud &
 nm-applet &
-python ~/.software/mpdforward/mpdforward.py 2>&1 |tee /home/bart/tmp/log/mpdforward.log &
 bash ~/.config/notebookConfig/scripts/setBg.sh
--- scripts/mpc.sh
+++ scripts/mpc.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+source ~/.mpcrc
 mpc $@
 pkill -RTMIN+2 i3blocks
